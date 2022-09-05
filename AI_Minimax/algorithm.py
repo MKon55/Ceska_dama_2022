fd9f03f53040aebc9da3ad0321f91adbdb0388b8
Michal Konrád <misapost@seznam.cz>
2022-09-05 17:15:48 +0200

    Ready for AI

--- AI_Minimax/algorithm.py
+++ AI_Minimax/algorithm.py
@@ -4,31 +4,41 @@
 from copy import deepcopy
 from game.stat_values import BLACK, WHITE
 
-#Minimax method
-def minimax(position, depth, max_player, game):
+AI_color = BLACK
+Player_color = WHITE
+
+#Minimax method + alpha-beta pruning => makes computing run faster
+def minimax(position, depth, alpha, beta, max_player, game):
     #position => current board position, object
     if depth == 0 or position.Winner() != None:
         return position.evaluate(), position
 
     if max_player:
-        maxEvaluate = float('-100') #If we haven't found anything yet, it's -inf
+        maxEvaluate = float('-inf') #If we haven't found anything yet, it's -inf
         best_move = None #Stores best move we can make
-        for move in get_all_moves(position, BLACK, game): #For every move eval => calls minimax (reculsive)
-            evaluation = minimax(move, depth-1, False, game)[0] #returns board and value
+        for move in get_all_moves(position, AI_color, game): #For every move eval => calls minimax (reculsive)
+            evaluation = minimax(move, depth-1, alpha, beta, False, game)[0] #returns board and value
             maxEvaluate = max(maxEvaluate, evaluation)
+            alpha = max(alpha, evaluation)
             if maxEvaluate == evaluation:
                 best_move = move #Sets current found best move in board as a best move
+            if beta <= alpha:
+                break
 
         return maxEvaluate, best_move
+    
     else:
-        minEvaluate = float('100') #If we haven't found anything yet, it's inf
+        minEvaluate = float('inf') #If we haven't found anything yet, it's inf
         best_move = None #Stores best move we can make
-        for move in get_all_moves(position, WHITE, game): #For every move eval => calls minimax (reculsive)
-            evaluation = minimax(move, depth-1, True, game)[0] #returns board and value
+        for move in get_all_moves(position, Player_color, game): #For every move eval => calls minimax (reculsive)
+            evaluation = minimax(move, depth-1, alpha, beta, True, game)[0] #returns board and value
             minEvaluate = min(minEvaluate, evaluation)
+            beta = min(beta, evaluation)
             if minEvaluate == evaluation:
                 best_move = move #Sets current found best move in board as a best move
-
+            if beta <= alpha:
+                break
+            
         return minEvaluate, best_move
 
 #Method for move simulation on temp board
